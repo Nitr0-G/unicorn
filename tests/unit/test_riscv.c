d5e72b42619159ccd9db8503472c7674e5e8d1a9
Marius <marius@users.noreply.github.com>
2026-06-22 09:54:12 +0300

    issue-2314 fixed
    Fix RISC-V single-precision NaN-boxing semantics.
    
    Issue: https://github.com/unicorn-engine/unicorn/issues/2314

--- tests/unit/test_riscv.c
+++ tests/unit/test_riscv.c
@@ -372,6 +372,92 @@ static void test_riscv64_fp_move_to_int(void)
     uc_close(uc);
 }
 
+static void test_riscv64_fclass_s_nanboxing(void)
+{
+    uc_engine *uc;
+    char code[] = "\xd3\x95\x07\xe0"; /* fclass.s a1, f15 */
+    uint64_t f15 = 0xffffffff7f800000ULL;
+    uint64_t a1 = 0;
+    uint64_t mstatus = 0x6000;
+
+    uc_common_setup(&uc, UC_ARCH_RISCV, UC_MODE_RISCV64, code,
+                    sizeof(code) - 1);
+    OK(uc_reg_write(uc, UC_RISCV_REG_MSTATUS, &mstatus));
+    OK(uc_reg_write(uc, UC_RISCV_REG_F15, &f15));
+
+    OK(uc_emu_start(uc, code_start, code_start + sizeof(code) - 1, 0, 1));
+    OK(uc_reg_read(uc, UC_RISCV_REG_A1, &a1));
+    TEST_CHECK(a1 == 0x80);
+
+    f15 = 0x62bf9dd562bf9dd5ULL;
+    a1 = 0;
+    OK(uc_reg_write(uc, UC_RISCV_REG_F15, &f15));
+    OK(uc_reg_write(uc, UC_RISCV_REG_A1, &a1));
+
+    OK(uc_emu_start(uc, code_start, code_start + sizeof(code) - 1, 0, 1));
+    OK(uc_reg_read(uc, UC_RISCV_REG_A1, &a1));
+    TEST_CHECK(a1 == 0x200);
+
+    f15 = 0xbc26093bbc260929ULL;
+    a1 = 0;
+    OK(uc_reg_write(uc, UC_RISCV_REG_F15, &f15));
+    OK(uc_reg_write(uc, UC_RISCV_REG_A1, &a1));
+
+    OK(uc_emu_start(uc, code_start, code_start + sizeof(code) - 1, 0, 1));
+    OK(uc_reg_read(uc, UC_RISCV_REG_A1, &a1));
+    TEST_CHECK(a1 == 0x200);
+
+    OK(uc_close(uc));
+}
+
+static void test_riscv64_fclass_s_produced_nanbox(void)
+{
+    uc_engine *uc;
+    char code[] = "\xd3\x07\x05\xd0\xd3\x95\x07\xe0";
+    uint64_t a0 = 1;
+    uint64_t a1 = 0;
+    uint64_t f15 = 0;
+    uint64_t mstatus = 0x6000;
+
+    uc_common_setup(&uc, UC_ARCH_RISCV, UC_MODE_RISCV64, code,
+                    sizeof(code) - 1);
+    OK(uc_reg_write(uc, UC_RISCV_REG_MSTATUS, &mstatus));
+    OK(uc_reg_write(uc, UC_RISCV_REG_A0, &a0));
+
+    OK(uc_emu_start(uc, code_start, code_start + sizeof(code) - 1, 0, 2));
+    OK(uc_reg_read(uc, UC_RISCV_REG_A1, &a1));
+    OK(uc_reg_read(uc, UC_RISCV_REG_F15, &f15));
+
+    TEST_CHECK(a1 == 0x40);
+    TEST_CHECK(f15 == 0xffffffff3f800000ULL);
+
+    OK(uc_close(uc));
+}
+
+static void test_riscv64_fmv_w_x_nanbox(void)
+{
+    uc_engine *uc;
+    char code[] = "\xd3\x07\x05\xf0\xd3\x95\x07\xe0";
+    uint64_t a0 = 0x7f800000;
+    uint64_t a1 = 0;
+    uint64_t f15 = 0;
+    uint64_t mstatus = 0x6000;
+
+    uc_common_setup(&uc, UC_ARCH_RISCV, UC_MODE_RISCV64, code,
+                    sizeof(code) - 1);
+    OK(uc_reg_write(uc, UC_RISCV_REG_MSTATUS, &mstatus));
+    OK(uc_reg_write(uc, UC_RISCV_REG_A0, &a0));
+
+    OK(uc_emu_start(uc, code_start, code_start + sizeof(code) - 1, 0, 2));
+    OK(uc_reg_read(uc, UC_RISCV_REG_A1, &a1));
+    OK(uc_reg_read(uc, UC_RISCV_REG_F15, &f15));
+
+    TEST_CHECK(a1 == 0x80);
+    TEST_CHECK(f15 == 0xffffffff7f800000ULL);
+
+    OK(uc_close(uc));
+}
+
 static void test_riscv64_code_patching(void)
 {
     uc_engine *uc;
@@ -809,6 +895,10 @@ TEST_LIST = {
     {"test_riscv64_fp_move_from_int_reg_write",
      test_riscv64_fp_move_from_int_reg_write},
     {"test_riscv64_fp_move_to_int", test_riscv64_fp_move_to_int},
+    {"test_riscv64_fclass_s_nanboxing", test_riscv64_fclass_s_nanboxing},
+    {"test_riscv64_fclass_s_produced_nanbox",
+     test_riscv64_fclass_s_produced_nanbox},
+    {"test_riscv64_fmv_w_x_nanbox", test_riscv64_fmv_w_x_nanbox},
     {"test_riscv64_ecall", test_riscv64_ecall},
     {"test_riscv32_mmio_map", test_riscv32_mmio_map},
     {"test_riscv64_mmio_map", test_riscv64_mmio_map},
